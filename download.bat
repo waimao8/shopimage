4b23c34beb0a5efea6a9d04153db8b74e82004c0
serbia580 <serbia580@gmail.com>
2024-09-24 12:47:44 +0200

    “更新文件”

--- download.bat
+++ download.bat
@@ -1,5 +1,5 @@
 @echo off
 echo 正在下载文件从GitHub…
 git pull
-echo 上传完成！
+echo 下载完成！
 pause
---
+++ 克隆.bat
@@ -0,0 +1,6 @@
+@echo off
+cd E:\shop\china\beifeng
+echo 正在下载文件从GitHub…
+git clone git@github.com:waimao8/shopimage.git
+echo 克隆完成！
+pause
