077eba841d06e2af11ccef5a95c4055410269c30
serbia580 <serbia580@gmail.com>
2024-09-24 11:20:27 +0200

    Upload files

---
+++ upload.bat
@@ -0,0 +1,5 @@
+@echo off
+cd E:\shop\china\image
+git add .
+git commit -m ¡°Upload files¡±
+git push -u origin main
---
+++ upload.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd  E:\shop\china\image
+git add .
+git commit -m "upload"
+git push -u origin main
